237f4ff62ec86122d486fa7237f22a6d7797cd65
Thijs Busser <thijs.busser@iodigital.com>
2023-12-09 16:34:00 +0100

    Add the solution for 9.1

---
+++ src/09/part-1.ts
@@ -0,0 +1,61 @@
+import { fetchInputForDay } from '../utils/fetch-input.js';
+import { measure } from '../utils/performance.js';
+
+/* ========================================================================== */
+
+function calculateTotal(values: number[]): number {
+	return values.reduce((total, value) => total + value, 0);
+}
+
+function findNextValue(history: string): number {
+	// Convert the history to an array of values.
+	let current: number[] = history.split(' ').map(Number);
+	// Keep track of the last value for each iteration, start with the last
+	// value of the input.
+	const lastValues = [current.at(-1)];
+
+	// Keep iterating until the difference between all the values is 0 or there
+	// is only one value left. At this point we've found all the sequences.
+	do {
+		// Create a copy of the array to work with.
+		const input = [...current];
+		// Clear the current array.
+		current = [];
+		// Iterate over all the values in the array, except the last one as
+		// there is no next number to calculate the difference with.
+		for (let index = 0; index < input.length - 1; index++) {
+			// Push the difference between the value to the current array.
+			current.push(input[index + 1] - input[index]);
+		}
+		// Keep the last value in the array, this will be needed to predict the
+		// next value in the sequence.
+		lastValues.push(current.at(-1));
+	} while (current.some(value => value !== 0) && current.length > 1);
+
+	// Add up all the last values of the generated sequences, the sum is the
+	// predicted next value based on the history.
+	return calculateTotal(lastValues);
+}
+
+/* ========================================================================== */
+
+async function findSolution(input: string): Promise<number> {
+	const histories = input.split('\n');
+
+	// Calculate the predicted next value for each history line.
+	const predictions = histories.map(findNextValue);
+
+	// Return the sum of all the predicted values.
+	return calculateTotal(predictions);
+}
+
+/* ========================================================================== */
+
+// Get the input for the puzzle.
+const rawInput = await fetchInputForDay(9);
+const result = await measure(() => findSolution(rawInput));
+
+/* -------------------------------------------------------------------------- */
+
+console.log(`The sum of the extrapolated values is: ${result.answer}`);
+console.log(`Time taken: ${result.duration}ms`);
